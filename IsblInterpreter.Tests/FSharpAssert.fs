ddcf07956f5be5ee1394a5a03780e4925f5e1bc3
Kurnosov Nikolay <niqolay@gmail.com>
2011-05-27 01:54:59 +0400

    Forgotten files

---
+++ IsblInterpreter.Tests/FSharpAssert.fs
@@ -0,0 +1,18 @@
+﻿#light
+
+module FSharpAssert
+
+let throws command =
+  try
+    do command()
+    false
+  with
+    | _ -> true 
+
+let throwsSpecific(command, exceptionType: System.Type) =
+  try
+    do command()
+    false
+  with
+    | err -> exceptionType.IsAssignableFrom(err.GetType())
+
---
+++ IsblInterpreter.Tests/IfStatement.fs
@@ -0,0 +1,50 @@
+﻿#light
+
+module IfStatement
+
+open Xunit
+open Evaluator
+
+[<Fact>]
+let IfOnTrueVariableExecutesIfBody() =
+  let sample = @"
+    a := 1;
+    if (a)
+      a := 3
+    endif"   
+  let env = interpret sample
+  Assert.True(env.TryFind("a") = Some(3))
+
+[<Fact>]
+let IfOnFalseVariableExecutesIfBody() =
+  let sample = @"
+    a := 0;
+    if (a)
+      a := 3
+    endif"   
+  let env = interpret sample
+  Assert.True(env.TryFind("a") = Some(0))
+
+[<Fact>]
+let IfBodyCanContainStatementList() =
+  let sample = @"
+    a := 1;
+    if (a)
+      a := 3;
+      a := 4
+    endif"   
+  let env = interpret sample
+  Assert.True(env.TryFind("a") = Some(4))
+
+[<Fact>]
+let IfOnFalseExecutesElseBody() =
+  let sample = @"
+    a := 0;
+    if (a)
+      a := 3
+    else
+      a := 4
+    endif"   
+  let env = interpret sample
+  Assert.True(env.TryFind("a") = Some(4))
+
